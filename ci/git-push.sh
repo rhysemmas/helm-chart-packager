9fc881756e5e2f5ee5db209376a1ef2d39af8875
Rhys Emmas <rhys.emmas@uswitch.com>
2019-08-14 11:00:25 +0100

    populate repo url

--- ci/git-push.sh
+++ ci/git-push.sh
@@ -3,7 +3,7 @@
 mkdir ${DRONE_WORKSPACE}/new-repo/ && cd ${DRONE_WORKSPACE}/new-repo/
 git init
 git config --global user.email $GIT_EMAIL
-git remote add origin https://$GIT_USER:$GIT_TOKEN@github.com/user/helm-chart-repo.git
+git remote add origin https://$GIT_USER:$GIT_TOKEN@github.com/rhysemmas/helm-chart-packager.git
 git fetch
 git checkout --track origin/master
 git pull
