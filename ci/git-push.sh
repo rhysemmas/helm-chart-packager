cde78a656cb5ca914503e42f0c79b0277980ac95
Rhys Emmas <rhys.emmas@uswitch.com>
2019-08-14 12:12:32 +0100

    more pipeline changes

--- ci/git-push.sh
+++ ci/git-push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 cd ${DRONE_WORKSPACE}/new-repo/
+git config --global user.email ${GIT_EMAIL}
+git remote add origin https://${GIT_USER}:${GIT_TOKEN}@github.com/rhysemmas/helm-chart-hoster.git
 git add .
 git commit -m "Helm package robot commit"
 git push -u origin master
