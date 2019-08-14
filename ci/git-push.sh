4f9bf0e4a511c357c2d6922796c31f69a6d3966f
Rhys Emmas <rhys.emmas@uswitch.com>
2019-08-14 11:01:23 +0100

    fix url

--- ci/git-push.sh
+++ ci/git-push.sh
@@ -3,7 +3,7 @@
 mkdir ${DRONE_WORKSPACE}/new-repo/ && cd ${DRONE_WORKSPACE}/new-repo/
 git init
 git config --global user.email $GIT_EMAIL
-git remote add origin https://$GIT_USER:$GIT_TOKEN@github.com/rhysemmas/helm-chart-packager.git
+git remote add origin https://$GIT_USER:$GIT_TOKEN@github.com/rhysemmas/helm-chart-hoster.git
 git fetch
 git checkout --track origin/master
 git pull
