98b45648af879020471ed95aababda41791e7012
Rhys Emmas <rhys.emmas@uswitch.com>
2019-08-14 09:34:57 +0100

    update access token

--- ci/git-push.sh
+++ ci/git-push.sh
@@ -3,11 +3,11 @@
 mkdir ${DRONE_WORKSPACE}/new-repo/ && cd ${DRONE_WORKSPACE}/new-repo/
 git init
 git config --global user.email $GIT_EMAIL
-git remote add origin https://$GIT_USER:$GIT_PWD@github.com/rhysemmas/helm-chart-hoster.git
+git remote add origin https://$GIT_USER:$GIT_TOKEN@github.com/user/helm-chart-repo.git
 git fetch
 git checkout --track origin/master
 git pull
 mv ${DRONE_WORKSPACE}/output/* ${DRONE_WORKSPACE}/new-repo/charts/
 git add .
 git commit -m "Helm package robot commit" && echo "Local files committed" || echo "Error committing files"
-git push -u origin master && echo "Local files pushed to remote" || echo "Error pushing to remote" 
+git push -u origin master && echo "Local files pushed to remote" || echo "Error pushing to remote"
